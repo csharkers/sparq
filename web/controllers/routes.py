6e0185614c41dbb32d1f6ced87d4c6e042fffd01
Davi Kirk <davikirk2008@gmail.com>
2025-05-16 15:27:00 -0300

    create aside page

--- web/controllers/routes.py
+++ web/controllers/routes.py
@@ -1,22 +1,19 @@
 from flask import render_template, request
 
+
 def init_app(app):
 
     @app.route('/')
-     
     def home():
 
         return render_template('index.html',)
-    
-<<<<<<< HEAD
+
     @app.route('/menu')
     def menu():
 
         return render_template('menu.html',)
-=======
+
     @app.route('/loginPage')
     def loginPage():
 
         return render_template('login.html',)
->>>>>>> feature/login-page
-       
