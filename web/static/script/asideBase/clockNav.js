67d96793d1e441101b9aabb68962beb1ab5a6487
DaviKirk <davikirk2008@gmail.com>
2025-05-23 12:28:27 -0300

    nav and aside finished

--- web/static/script/asideBase/clockNav.js
+++ web/static/script/asideBase/clockNav.js
@@ -1,13 +1,41 @@
-const clockTime = document.getElementsByClassName("clock-time");
+const clockTime = document.getElementById("clock-time");
+const date = document.getElementById('date-day')
+
+const today = new Date();
+
+clockTime.innerHTML = "00:00"
 
 function updateTime(){
-
- const today = new Date();
 
  let H = today.getHours();
  let M = today.getMinutes();
 
  console.log( H, M );
+
+ hours = H < 10 ? '0' + H : H;
+ minutes = M < 10 ? '0' + M : M;
+
+ clockTime.innerHTML = `${hours}:${minutes}`;
+
+}
+
+function updateday(){
+
+    D = today.getDate();
+    M = today.getMonth();
+    year = today.getFullYear();
+
+     M =  M + 1;
+ 
+     month = M < 10 ? '0' + M : M;
+     day = D < 10? '0' + D : D;
+
+    date.innerHTML = `${day}/${month}/${year}`;
+
 }
 
 updateTime()
+updateday()
+
+setInterval(updateday, 1000);
+setInterval(updateTime, 1000);
