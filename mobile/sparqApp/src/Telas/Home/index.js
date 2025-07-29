8e834e160b8c05986ccbcdb06011dd359140b078
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-07-29 14:40:13 -0300

    styling the home screen

--- mobile/sparqApp/src/Telas/Home/index.js
+++ mobile/sparqApp/src/Telas/Home/index.js
@@ -1,25 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons'
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.navbar}>
         <View>
-          {/* <Image source={require("")}></Image> */}
+          {/* <TouchableOpacity>
+            <Ionicons name="menu" size={24} color="white" />
+          </TouchableOpacity> */}
         </View>
         <View>
-          <Text></Text>
-          <Text></Text>
+          <Image style={styles.imageProfile} source={require("../../../assets/profileIMG.jpg")}></Image>
         </View>
         <View>
-          <View>
-            <ion-icon name="map-outline"></ion-icon>
-            <Text>Mapa de sensores</Text>
-          </View>
+          <Text style={styles.textName}>Davi Lambari</Text>
+          <Text style={styles.textFunction}>Guarda mata</Text>
         </View>
       </View>
+      <View style={styles.buttonContainer} >
+        <TouchableOpacity style={styles.button}>
+          <Ionicons style={styles.iconButton} name="map" size={24} color="black" />
+          <Text style={styles.textButton}>Mapa de sensores</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons style={styles.iconButton} name="server" size={24} color="black" />
+          <Text style={styles.textButton}>informação dos sensores</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -29,6 +40,49 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  navbar: {
+    display: "flex",
+    backgroundColor: '#111111',
+    width: "100%",
+    height: "30%",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  button: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    padding: 10,
+    alignItems: "center"
+  },
+  iconButton: {
+    fontSize: 50
+  },
+  textButton: {
+    padding: 10,
+    fontSize: 20
+  },
+  textName: {
+    color: "#fff",
+    fontSize: 30
+  },
+  textFunction: {
+    color: "#fff",
+    fontSize: 15
+  },
+  buttonContainer: {
+    display: "flex",
+    width: "100%",
+    alignItems: "center",
+    textAlign: "center"
+  },
+  imageProfile: {
+    width: 90,
+    height: 90,
+    borderRadius: 100,
+    marginRight: 20
+  }
 });
