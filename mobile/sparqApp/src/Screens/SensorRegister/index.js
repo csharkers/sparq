fa988671a59e50f7baecbb239c922269f2928c30
DaviKirk <davikirk2008@gmail.com>
2025-08-31 16:08:27 -0300

    function getLocation adding

--- mobile/sparqApp/src/Screens/SensorRegister/index.js
+++ mobile/sparqApp/src/Screens/SensorRegister/index.js
@@ -1,31 +1,15 @@
-import { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert } from "react-native";
+import * as Location from "expo-location";
+import { getLocation } from "../../services/sensorRegister"
 
-//import da função que puxa a localização
-import { getLocation } from '../../services/sensorRegister';
-
-export default function App() {
-
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  const handleGetLocation = async () => {
-    try {
-      const loc = await getLocation();
-      setLocation(loc);
-    } catch (error) {
-      setErrorMsg(error.message);
-    }
-  };
+export default function TelaSensor() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={getLocation}>
-        <Text>Get Location</Text>
+      <TouchableOpacity onPress={() => getLocation()}>
+        <Text>PEGAR localização</Text>
       </TouchableOpacity>
-      <Text style={styles.paragraph}>
-         {errorMsg ? errorMsg : location ? JSON.stringify(location) : "Sem dados"}
-      </Text>
     </View>
   );
 }
@@ -33,12 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 20,
-  },
-  paragraph: {
-    fontSize: 18,
-    textAlign: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
 });
--- mobile/sparqApp/src/services/sensorRegister.js
+++ mobile/sparqApp/src/services/sensorRegister.js
@@ -1,15 +1,28 @@
 import api from "./api";
 import { useState, useEffect } from 'react';
+import * as Location from "expo-location";
+import { Alert } from "react-native";
 
 
 export const getLocation = async () => {
 
-    let { status } = await Location.requestForegroundPermissionsAsync();
-
-    if( status != 'grated'){
-        throw new Error('Permissão de localização negada');
+    //verifica se o serviço de loc esta ligado
+    const service = await Location.hasServicesEnabledAsync();
+    if (!service) {
+        Alert.alert("GPS desligado", "Ative a localização do dispositivo e tente novamente");
+        return;
     }
+    const { status, canAskAgain } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+        if (canAskAgain) {
+            Alert.alert("Permissão negada", "tente novamente")
+        }
+        return;
+    }
+    const pos = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Highest,
+    });
 
-    let currentLocation = await Location.getCurrentPositionAsync({});
-    return currentLocation
+    console.log(pos)
+
 };
